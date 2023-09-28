3dedac78755670d9b3feb30a078e449e82e303a5
DanielGarcia <dgayala002@gmail.com>
2023-09-28 19:40:05 +0200

    Creada tabla

--- index.php
+++ index.php
@@ -1,2 +1,80 @@
 <?php
 include('data.php')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horario</title>
+</head>
+<body>
+    <table>
+            <tr>
+                <th>HORAS</th>
+                <th>LUNES</th>
+                <th>MARTES</th>
+                <th>MIÉRCOLES</th>
+                <th>JUEVES</th>
+                <th>VIERNES</th>
+            </tr>
+        <tbody>
+            <tr>
+                <td>16:00 - 16:55</td>
+                <td>Fila 1</td>
+                <td>Fila 1</td>
+                <td>Fila 1</td>
+                <td>Fila 1</td>
+                <td>Fila 1</td>
+            </tr>
+            <tr>
+                <td>16:55 - 17:50</td>
+                <td>Fila 2</td>
+                <td>Fila 2</td>
+                <td>Fila 2</td>
+                <td>Fila 2</td>
+                <td>Fila 2</td>
+            </tr>
+            <tr>
+                <td>17:50 - 18:45</td>
+                <td>Fila 3</td>
+                <td>Fila 3</td>
+                <td>Fila 3</td>
+                <td>Fila 3</td>
+                <td>Fila 3</td>
+            </tr>
+            <tr>
+                <td>18:45 - 19:10</td>
+                <td>Fila 4</td>
+                <td>Fila 4</td>
+                <td>Fila 4</td>
+                <td>Fila 4</td>
+                <td>Fila 4</td>
+            </tr>
+            <tr>
+                <td>19:10 - 20:05</td>
+                <td>Fila 5</td>
+                <td>Fila 5</td>
+                <td>Fila 5</td>
+                <td>Fila 5</td>
+                <td>Fila 5</td>
+            </tr>
+            <tr>
+                <td>20:05 - 21:00</td>
+                <td>Fila 6</td>
+                <td>Fila 6</td>
+                <td>Fila 6</td>
+                <td>Fila 6</td>
+                <td>Fila 6</td>
+            </tr>
+            <tr>
+                <td>21:00 - 21:55</td>
+                <td>Fila 7</td>
+                <td>Fila 7</td>
+                <td>Fila 7</td>
+                <td>Fila 7</td>
+                <td>Fila 7</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
